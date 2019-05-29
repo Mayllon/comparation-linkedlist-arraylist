b2f7c1df15b09e8f791a203dcf9c7618b2fc8848
mayllon <mayllonbaumer@gmail.com>
2019-05-29 21:25:44 +0100

    Adding source

---
+++ src/main/java/com/foodmarket/ComparationLinkedlistArraylistApplication.java
@@ -0,0 +1,13 @@
+package com.foodmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparationLinkedlistArraylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparationLinkedlistArraylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodmarket/ComparationLinkedlistArraylistApplicationTests.java
@@ -0,0 +1,78 @@
+package com.foodmarket;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ComparationLinkedlistArraylistApplicationTests {
+
+	public static final int QUANTITY_OBJECT = 10000000;
+	public static final int INDEX_ARRAY = 888888;
+	static ArrayList<Integer> arrayListOfInteger;
+	static LinkedList<Integer> linkedListOfInteger;
+
+	@BeforeClass
+	public static void load() {
+		arrayListOfInteger = new ArrayList<Integer>();
+		linkedListOfInteger = new LinkedList<Integer>();
+		for(int i = 0; i < 1000000;i++) {
+			arrayListOfInteger.add(i);
+			linkedListOfInteger.add(i);
+		}
+	}
+	
+	@Test
+	public void addObjectArrayList() {
+		arrayListOfInteger = new ArrayList<Integer>();
+		for(int i = 0; i < QUANTITY_OBJECT;i++) {
+			arrayListOfInteger.add(i);
+		}
+	}
+	@Test
+	public void addObjectLinkedList() {
+		linkedListOfInteger = new LinkedList<Integer>();
+		for(int i = 0; i < QUANTITY_OBJECT;i++) {
+			linkedListOfInteger.add(i);
+		}
+	}
+	@Test
+	public void insertObjectArrayList() {
+		for(int i = 0; i < 100;i++)
+			arrayListOfInteger.add(INDEX_ARRAY + i, 900);
+	}
+	@Test
+	public void insertObjectLinkedList() {
+		for(int i = 0; i < 100;i++)
+			linkedListOfInteger.add(INDEX_ARRAY + i, 900);
+	}
+	@Test
+	public void removeObjectArrayList() {
+		for(int i = 0; i < 100;i++)
+			arrayListOfInteger.remove(INDEX_ARRAY + i);
+	}
+	@Test
+	public void removeObjectLinkedList() {
+		for(int i = 0; i < 100;i++)
+			linkedListOfInteger.remove(INDEX_ARRAY + i);
+	}
+	@Test
+	public void replaceObjectArrayList() {
+		for(int i = 0; i < 100;i++)
+			arrayListOfInteger.set(INDEX_ARRAY + i, 88888);
+	}
+	@Test
+	public void replaceObjectLinkedList() {
+		for(int i = 0; i < 100;i++)
+			linkedListOfInteger.set(INDEX_ARRAY + i, 88888);
+	}
+}
